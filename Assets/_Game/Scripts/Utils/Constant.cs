bcfadc4333ccd3eda7fcf8c8ef705a17c8c5163f
NotleAnkle <phamdacanh2k2@gmail.com>
2024-02-12 20:40:16 +0700

    + add utuls scripts

---
+++ Assets/_Game/Scripts/Utils/Constant.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constant 
+{
+
+    public const string TAG_PLAYER = "Player";
+    public const string TAG_PLAYER_CLONE = "PlayerClone";
+
+    public const string ANIM_WALK_UP = "walk_up";
+    public const string ANIM_WALK_DOWN = "walk_down";
+    public const string ANIM_WALK_SIDE = "walk_side";
+    public const string ANIM_IDLE = "idle";
+    public const string ANIM_HIT = "hit";
+
+
+}
---
+++ Assets/_Game/Scripts/Utils/FollowingCamera.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowingCamera : MonoBehaviour
+{
+    public GameObject target;
+    [SerializeField] private new Camera camera;
+
+    private Vector3 offset;
+
+    private void Awake()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    public void OnInit()
+    {
+    }
+
+
+    void Update()
+    {
+        transform.position = target.transform.position + offset;
+    }
+
+}

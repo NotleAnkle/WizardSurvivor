74fc42ca2cb7e9c4fa6d619f1b44f23f6216b75f
NotleAnkle <phamdacanh2k2@gmail.com>
2024-02-14 23:01:48 +0700

    + add constant vaule

--- Assets/_Game/Scripts/Utils/Constant.cs
+++ Assets/_Game/Scripts/Utils/Constant.cs
@@ -7,6 +7,8 @@ public class Constant
 
     public const string TAG_PLAYER = "Player";
     public const string TAG_PLAYER_CLONE = "PlayerClone";
+    public const string TAG_ENEMY = "Enemy";
+    public const string TAG_BULLET = "Bullet";
 
     public const string ANIM_WALK_UP = "walk_up";
     public const string ANIM_WALK_DOWN = "walk_down";
@@ -14,5 +16,9 @@ public class Constant
     public const string ANIM_IDLE = "idle";
     public const string ANIM_HIT = "hit";
 
+    public const float ORB_COOLDOWN = 1f;
+    public const float ORB_RANGE_DEFAULT = 5f;
+
+    public const float ENEMY_SKULL_HP = 3f;
 
 }

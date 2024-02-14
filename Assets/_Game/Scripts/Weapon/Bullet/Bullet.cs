8b273caf229b30e8aa292019e404dcd40c8c317e
NotleAnkle <phamdacanh2k2@gmail.com>
2024-02-14 23:02:20 +0700

    - update bullet effect

--- Assets/_Game/Scripts/Weapon/Bullet/Bullet.cs
+++ Assets/_Game/Scripts/Weapon/Bullet/Bullet.cs
@@ -1,3 +1,4 @@
+using _Framework;
 using _Framework.Pool.Scripts;
 using System.Collections;
 using System.Collections.Generic;
@@ -6,24 +7,52 @@ using UnityEngine;
 public class Bullet : GameUnit
 {
     [SerializeField] private GameUnit target;
-    [SerializeField] private AnimationCurve curve = new AnimationCurve();
     [SerializeField] private float speed = 1f;
-    private float timer = 0f;
-    private void Update()
+    [SerializeField] private float damage = 1f;
+    [SerializeField] private SpriteRenderer img;
+    [SerializeField] private TrailRenderer trail;
+
+    private bool isHit = false;
+
+    public void OnInit(GameUnit target)
     {
-        Move();
+        trail.Clear();
+        img.enabled = true;
+        isHit = false;
+        this.target = target;
     }
 
-    private void Start()
+    private void Update()
     {
-
+        if(!isHit) Move();
     }
 
     private void Move()
     {
-        float curveValue = curve.Evaluate(timer);
-        TF.position = Vector3.MoveTowards(TF.position, target.TF.position, speed * Time.deltaTime * curveValue);
-        timer += Time.deltaTime;
+        TF.position = Vector3.MoveTowards(TF.position, target.TF.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(Constant.TAG_ENEMY)) 
+        {
+            OnDespawn();
+            Enemy enemy = Cache<Enemy>.GetComponent(collision);
+            isHit = true;
+            enemy.OnHit(damage);
+        }
+    }
+
+    private void OnDespawn()
+    {
+        img.enabled = false;
+        StartCoroutine(DelayDespawn());
+    }
+
+    private IEnumerator DelayDespawn()
+    {
+        yield return new WaitForSeconds(0.1f);
+        SimplePool.Despawn(this);
     }
 }
 
